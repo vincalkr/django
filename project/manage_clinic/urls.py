4849377038b01d9824503c7b3becfcda8a0510a0
Vincenzo Calendini <vincenzocalendini@gmail.com>
2022-06-19 22:04:11 +0200

    update

--- project/manage_clinic/urls.py
+++ project/manage_clinic/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('exam/', views.index, name='index'),
+    path('exam/detail/<int:exam_id>', views.exam, name='exam'),
 ]
--- project/manage_clinic/views.py
+++ project/manage_clinic/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request: Request) -> HttpResponse:
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def exam(request: Request, ) -> HttpResponse:
+    return HttpResponse("Hello, world. You're at the polls index.")
