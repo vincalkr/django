bc5ec97089b548081c015a62ef99ba4329086bb8
Vincenzo Calendini <vincenzocalendini@gmail.com>
2022-06-29 20:53:09 +0200

    updates

--- clinic/urls.py
+++ clinic/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('example', views.template_example, name='example'),
-    path('exams/', views.exams, name='exams'),
-    path('exams/<int:pk>/', views.exam_detail, name='exam_detail'),
+    # path('exams/', views.exams_json, name='exams'),
+    path('exams/', views.ExamListView.as_view(), name='exam_list'),
+    path('exams/<int:pk>/', views.exam_detail_json, name='exam_detail'),
 ]
--- clinic/views.py
+++ clinic/views.py
@@ -3,6 +3,7 @@ from django.core import serializers
 from django.forms import model_to_dict
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.views.generic.list import ListView
 
 from clinic.models import Exam
 
@@ -20,12 +21,17 @@ def template_example(request: HttpRequest) -> HttpResponse:
 
     return render(request, 'example.html', context)
 
-def exams(request: HttpRequest) -> HttpResponse:
+def exams_json(request: HttpRequest) -> HttpResponse:
     exams = Exam.objects.all()
     
     return HttpResponse(serializers.serialize('json', exams), content_type='application/json')
 
-def exam_detail(request: HttpRequest, pk: int) -> HttpResponse:
+def exam_detail_json(request: HttpRequest, pk: int) -> HttpResponse:
     exam: Exam = get_object_or_404(Exam, pk=pk)
     
     return HttpResponse(json.dumps(model_to_dict(exam)), content_type='application/json')
+
+## Test con class based views
+class ExamListView(ListView):
+    model = Exam
+    template_name = 'exam-list.html'
