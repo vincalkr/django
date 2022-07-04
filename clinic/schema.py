20ae1256f034331c323213b5d706a499a83fbaaf
Vincenzo Calendini <vincenzocalendini@gmail.com>
2022-07-04 08:20:09 +0200

    update

--- clinic/schema.py
+++ clinic/schema.py
@@ -6,8 +6,8 @@ from . import types
 @strawberry.type(description="Querable items")
 class Query:
     exam: types.Exam = strawberry_django.field()
-    exams: List[ types.Exam] = strawberry_django.field()
-    patients: List[ types.Patient] = strawberry_django.field()
+    exams: List[types.Exam] = strawberry_django.field()
+    patients: List[types.Patient] = strawberry_django.field()
     reservations: List[types.Reservation] = strawberry_django.field()
 
 schema = strawberry.Schema(query=Query)
